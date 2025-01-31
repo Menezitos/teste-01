229cf7df971ab9484279c32b6755a0d91f237c94
Vinicius Menezes <vsmenezes95@gmail.com>
2025-01-31 11:19:22 -0300

    Projeto teste didatico

---
+++ app.js
@@ -0,0 +1,73 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+exibirMensagemInicial();
+
+function exibirTextonaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
+}
+function exibirMensagemInicial() {
+    exibirTextonaTela("h1", "Jogo do Número Secreto");
+    exibirTextonaTela("p", "Escolha um número de 1 a 10:");
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        //console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
+
+}
+
+function verificarChute() {
+    let chute = document.querySelector("input").value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextonaTela("h1", "Acertou!");
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+        let mensagemTentativas = `Você descobriu o Número Secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextonaTela("p", mensagemTentativas);
+        document.getElementById("reiniciar").removeAttribute("disabled");
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextonaTela("p", "O Número Secreto é menor!");
+        } else {
+            exibirTextonaTela("p", "O Número Secreto é maior!");
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector("input");
+    chute.value = "";
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById("reiniciar").setAttribute("disabled", true);
+}
